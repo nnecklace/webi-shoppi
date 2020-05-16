951319941df10dc7e865f91cfbe4ba00c078e7a8
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-16 21:07:33 +0300

    add routes for products

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,19 @@
-from flask import render_template
+from flask import render_template, request, jsonify
+from src.models.products.products import Product
+from src.db import db
 from src import app
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/products", methods=["POST"])
+def product_create():
+    product = Product(request.form.get("name"), request.form.get("price"), request.form.get("quantity"))
+    db.session().add(product)
+    db.session().commit()
+    return "Success"
+
+@app.route("/products")
+def product_list():
+    return render_template("products/main.html")
