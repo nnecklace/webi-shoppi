a49679810f800f362472e88d4ff7896b68be1751
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-17 21:41:37 +0300

    add sqlite support

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,6 +5,9 @@ import os
 
 if os.environ.get("ENV") == "HEROKU":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+elif os.environ.get("ENV") == "SQLITE":
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres@localhost:5432/webi_shoppi"
     app.config["SQLALCHEMY_ECHO"] = True
