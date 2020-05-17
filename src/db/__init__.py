1a628263073082ea9f16355887571bcbba92ad49
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-17 18:48:12 +0300

    add heroku env var

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from src import app
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres@localhost:5432/webi_shoppi"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("ENV") == "HEROKU":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres@localhost:5432/webi_shoppi"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
