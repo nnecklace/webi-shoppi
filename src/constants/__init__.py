c584ff4fb21fdc6118530209467897ad66e796a7
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-23 18:05:22 +0300

    move constant values to own module

---
+++ src/constants/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+def _get_env():
+    return os.environ.get("ENV")
+
+def env_sqlite():
+    return _get_env() == "SQLITE"
+
+def env_production():
+    return _get_env() == "HEROKU"
+
+def env_db_url():
+    if env_production():
+        return os.environ.get("DATABASE_URL")
+    elif env_sqlite():
+        return "sqlite:///database.db"
+    else:
+        return "postgres://postgres@localhost:5432/webi_shoppi"
--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,15 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from src import app
+from src.constants import env_production, env_sqlite, env_db_url
 
-import os
+if not env_production():
+    app.config["SQLALCHEMY_ECHO"] = True
 
-if os.environ.get("ENV") == "HEROKU":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-elif os.environ.get("ENV") == "SQLITE":
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-    app.config["SQLALCHEMY_ECHO"] = True
-else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres@localhost:5432/webi_shoppi"
-    app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = env_db_url()
 
 db = SQLAlchemy(app)
