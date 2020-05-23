442748066047141f06c664cb649ecb51513e9260
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-23 14:55:09 +0300

    add registration form to layout and controller

--- src/controllers/authentication/authentication.py
+++ src/controllers/authentication/authentication.py
@@ -1,7 +1,8 @@
 from flask import render_template, request, redirect, url_for
+from src.forms.authentication import RegisterForm
 from src.db import db
 
 class AuthenticationController:
     @staticmethod
     def register():
-        return render_template("authentication/register.html")
+        return render_template("authentication/register.html", form = RegisterForm())
