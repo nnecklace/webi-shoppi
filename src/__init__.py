71e81f56b97452394727837eed35e60528266c07
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-24 15:14:50 +0300

    enable bycrypt

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from os import urandom
 
 app = Flask(__name__, template_folder='views')
@@ -10,14 +11,28 @@ from src.db import db
 from src.models import products, users
 from src import routes
 
+"""
+Login manager
+"""
 login_manager = LoginManager()
 
 login_manager.init_app(app)
-login_manager.login_message = "This is login message"
+login_manager.login_message = "This is login message" # remove?
 
 @login_manager.user_loader
 def load_user(user_id):
     return users.User.query.get(user_id)
+
+"""
+Password encryption
+"""
+crypto = Bcrypt(app)
+
+def encrypt(str):
+    return crypto.generate_password_hash(str, 13)
+
+def check(candidate):
+    return crypto.check_password_hash(encrypt(candidate), candidate)
 
 try: 
     db.create_all()
