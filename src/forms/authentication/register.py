0d8cbc0618dec3de56e4c687949c3d410e46ae28
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-24 16:54:47 +0300

    fix password regex and add view_data field to register form

--- src/forms/authentication/register.py
+++ src/forms/authentication/register.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, HiddenField, validators
 from src.constants import get_required_msg
 
 def validate_password(form, field):
@@ -12,8 +12,9 @@ class RegisterForm(FlaskForm):
     email = StringField("Sähköposti", [validators.InputRequired(message=get_required_msg()), validators.Email(message='Virheellinen sähköpostiosoite')])
     first_name = StringField("Etunimi", [validators.InputRequired(message=get_required_msg())])
     last_name = StringField("Sukunimi", [validators.InputRequired(message=get_required_msg())])
-    password = PasswordField("Salasana", [validators.InputRequired(message=get_required_msg()), validators.Regexp('0-9', message='Salasanan täytyy sisältää ainakin yksi numero'), validators.Length(min=8, message='Salasanan täytyy olla vähintään 8 merkkiä pitkä'), validate_password, validators.EqualTo('password_again', 'Salasanat eivät täsmää')])
+    password = PasswordField("Salasana", [validators.InputRequired(message=get_required_msg()), validators.Regexp('.*[0-9].*', message='Salasanan täytyy sisältää ainakin yksi numero'), validators.Length(min=8, message='Salasanan täytyy olla vähintään 8 merkkiä pitkä'), validate_password, validators.EqualTo('password_again', 'Salasanat eivät täsmää')])
     password_again = PasswordField("Salasana uudelleen", [validators.InputRequired(message=get_required_msg())])
+    view_data_field = HiddenField("_view_data")
 
     class Meta:
         csrf = False 
