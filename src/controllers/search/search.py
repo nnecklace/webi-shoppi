8a09263538fb31bddee9602d7aa42e28d4be9a5f
Niclas Forsman <niclas.forsman@futurice.com>
2020-05-31 17:33:26 +0300

    add find by criteria to product model and search controller

---
+++ src/controllers/search/search.py
@@ -0,0 +1,21 @@
+from flask import request, redirect, url_for
+from src.controllers.base import render
+from src.models.products import Product
+from src.models.categories import Category
+from src.forms.search import SearchForm
+
+class SearchController:
+    @staticmethod
+    def get():
+        search_form = SearchForm(request.args)
+        categories = Category.query.all()
+        search_form.categories.choices = list(map(lambda cat: (cat.id, cat.name), categories))
+        search_form.categories.choices.append((-1, "Kategoriton"))
+
+        return render("search/main.html", search_form = search_form, products = Product.find_by_criteria(
+                                                                                        search_form.name.data,
+                                                                                        list(map(lambda id: int(id), search_form.categories.data)),
+                                                                                        search_form.price.data,
+                                                                                        search_form.date_start.data,
+                                                                                        search_form.date_end.data,
+                                                                                        search_form.seller.data))
--- src/models/products/products.py
+++ src/models/products/products.py
@@ -48,7 +48,78 @@ class Product(db.Model):
         self.price = product.price.data
         self.quantity = product.quantity.data
         return self._commit("product " + str(self.id) + " update :")
-    
+
     def delete(self):
         db.session().delete(self)
         return self._commit("product " + str(self.id) + " delete :")
+
+    @staticmethod
+    def find_by_criteria(name, categories, price, published_start, published_end, seller):
+        params_dict = {}
+        stmt = ("SELECT products.id,"
+               " products.name,"
+               " products.created_at,"
+               " products.price,"
+               " products.quantity,"
+               " users.username"
+               " FROM products"
+               " LEFT JOIN categories_products"
+               " ON categories_products.product_id = products.id"
+               " LEFT JOIN categories"
+               " ON categories.id = categories_products.category_id"
+               " INNER JOIN users"
+               " ON users.id = products.user_id"
+               " WHERE products.quantity > 0")
+
+        if name:
+            stmt += " AND products.name LIKE :name"
+            params_dict["name"] = "%"+name+"%"
+
+        if price:
+            stmt += " AND price <= :price"
+            params_dict["price"] = price
+
+        if published_start and published_end:
+            stmt += " AND products.created_at BETWEEN :published_start AND :published_end"
+            params_dict["published_start"] = published_start.strftime("%Y-%m-%d 00:00:00")
+            params_dict["published_end"] = published_end.strftime("%Y-%m-%d 23:59:59")
+        elif published_start:
+            stmt +=  " AND products.created_at >= :published_start"
+            params_dict["published_start"] = published_start.strftime("%Y-%m-%d 00:00:00")
+        elif published_end:
+            stmt +=  " AND products.created_at <= :published_end"
+            params_dict["published_end"] = published_end.strftime("%Y-%m-%d 23:59:59")
+
+        if seller:
+            stmt += " AND users.username LIKE :seller"
+            params_dict["seller"] = "%"+seller+"%"
+
+        if -1 in categories:
+            stmt += " GROUP BY products.id, users.username"
+            stmt += " HAVING COUNT(categories_products.product_id) = 0"
+        elif len(categories) > 0:
+            subquery = " AND categories.id = ANY(SELECT categories.id FROM categories WHERE categories.id = :cat_0"
+            params_dict["cat_0"] = categories[0]
+            for indx in range(1, len(categories)):
+                subquery += " OR categories.id = :cat_"+str(indx)
+                params_dict["cat_"+str(indx)] = categories[indx]
+            subquery += ")"
+            stmt += subquery
+            stmt += " GROUP BY products.id, users.username"
+
+        stmt += " ORDER BY products.created_at DESC"
+
+        stmt = text(stmt).bindparams(**params_dict)
+
+        rows = db.engine.execute(stmt)
+        response = []
+
+        for row in rows:
+            response.append({"id": row[0],
+                             "name": row[1],
+                             "created_at": row[2],
+                             "price": row[3],
+                             "quantity": row[4],
+                             "user": {"username": row[5]}})
+
+        return response
