d3932ccaafade3cfbd55f7f68fda0e7dac4021c9
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-04 22:36:56 +0300

    check for user id in decorator

--- src/decorators/__init__.py
+++ src/decorators/__init__.py
@@ -8,8 +8,14 @@ def user_required(fn):
     def wrapper(*args, **kwargs):
         # Don't trust flask sessions or users
         # We could try and cache this value ?
-        user = User.query.filter_by(username = kwargs["username"]).first()
-        if not user.id == current_user.id:
+        user = None
+
+        if "username" in kwargs:
+            user = User.query.filter_by(username = kwargs["username"]).first()
+        else:
+            user = User.query.get(kwargs["id"])
+
+        if user == None or not user.id == current_user.id:
             return redirect(url_for('index'))
 
         return fn(*args, **kwargs)
