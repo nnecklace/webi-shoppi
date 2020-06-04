0ebc37617d80be07070ac8769dd96d4d02b918c9
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-04 16:14:40 +0300

    add form for user updating

--- src/forms/__init__.py
+++ src/forms/__init__.py
@@ -1,3 +1,4 @@
 from .authentication import LoginForm, RegisterForm
 from .search import SearchForm
 from .products import ProductForm
+from .users import UserForm
---
+++ src/forms/users/users.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, HiddenField, validators
+from src.constants import get_required_msg
+
+class UserForm(FlaskForm):
+    username = StringField("Käyttäjätunnus", [validators.InputRequired(message=get_required_msg()), validators.Length(min=4, max=150, message='Tunnus täytyy olla vähintään 4 merkkiä pitkä')])
+    email = StringField("Sähköposti", [validators.InputRequired(message=get_required_msg()), validators.Length(max=150, message='Sähköpostiosoite liian pitkä, maksimipituus 150 merkkiä'), validators.Email(message='Virheellinen sähköpostiosoite')])
+    first_name = StringField("Etunimi", [validators.InputRequired(message=get_required_msg()), validators.Length(max=150, message='Etunimesi on liian pitkä, maksimipituus 150 merkkiä')])
+    last_name = StringField("Sukunimi", [validators.InputRequired(message=get_required_msg()), validators.Length(max=150, message='Sukunimesi on liian pitkä, maksimipituus 150 merkkiä')])
+
+    class Meta:
+        csrf = False
