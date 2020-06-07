c44d3f7617703d70d832d382da84bd76a66a0c4d
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-07 17:45:49 +0300

    fix type bug and add max integer constant to forms

--- src/forms/products/products.py
+++ src/forms/products/products.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, IntegerField, widgets, SelectMultipleField
-from src.constants import get_required_msg
+from src.constants import get_required_msg, get_max_integer
 
 class MultiCheckbox(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
 class ProductForm(FlaskForm):
-    name = StringField("Nimi", [validators.Required(message=get_required_msg()), validators.Length(max=150, message="Tuotteen nimi on liian pitkä. Maksimipituus 150.")])
-    price = IntegerField("Hinta (€)", [validators.Required(message=get_required_msg()), validators.NumberRange(min=1, max=1073741824, message="Minimihinta on 1 € ja maksimihinta on 1073741824")])
-    quantity = IntegerField("Kpl määrä", [validators.NumberRange(min=1, max=1073741824, message="Kpl määrä täytyy olla vähintään yksi (1) ja saa maksimissaan olla 1073741824")])
+    name = StringField("Nimi", [validators.InputRequired(message=get_required_msg()), validators.Length(max=150, message="Tuotteen nimi on liian pitkä. Maksimipituus 150.")])
+    price = IntegerField("Hinta (€)", [validators.InputRequired(message=get_required_msg()), validators.NumberRange(min=1, max=get_max_integer(), message="Minimihinta on 1 € ja maksimihinta on " + str(get_max_integer()))])
+    quantity = IntegerField("Kpl määrä", [validators.NumberRange(min=1, max=get_max_integer(), message="Kpl määrä täytyy olla vähintään yksi (1) ja saa maksimissaan olla " + str(get_max_integer()))])
     categories = MultiCheckbox("Label", choices=[])
     
     class Meta:
--- src/forms/search/search.py
+++ src/forms/search/search.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, widgets, SelectMultipleField, BooleanField, validators
 from wtforms.fields.html5 import DateField
+from src.constants import get_max_integer
 
 class MultiCheckbox(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
@@ -8,7 +9,7 @@ class MultiCheckbox(SelectMultipleField):
 
 class SearchForm(FlaskForm):
     name = StringField("Tuotteen nimi", [validators.Length(max=150, message="Tuotteen nimi on liian pitkä. Maksimipituus 150 merkkiä")])
-    price = IntegerField("Hinta korkeintaan", [validators.NumberRange(max=1073741824, message="Maksimihinta on 1073741824")])
+    price = IntegerField("Hinta korkeintaan", [validators.NumberRange(max=get_max_integer(), message="Maksimihinta on " + str(get_max_integer()))])
     minimum = BooleanField("Halvin")
     date_start = DateField("Jälkeen", format="%Y-%m-%d")
     date_end = DateField("Ennen", format="%Y-%m-%d")
