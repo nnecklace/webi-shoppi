6d6590b2794364a7512d61543b95457999948805
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-10 23:43:59 +0300

    fix updating of category logic

--- src/models/categories_products/categories_products.py
+++ src/models/categories_products/categories_products.py
@@ -12,19 +12,29 @@ class CategoryProduct(db.Model):
     product_id = db.Column(db.Integer, db.ForeignKey("products.id", ondelete="CASCADE"), nullable=False)
 
     def add_product_categories(self, product_id, categories):
-        current_category_ids = list(map(lambda cat: cat.id,
-                                    Category.query.filter(Product.id == product_id).all()))
+        curr_product = Product.query.get(product_id)
 
-        categories_to_delete = CategoryProduct.query.filter(CategoryProduct.product_id == product_id).filter(CategoryProduct.category_id.in_(current_category_ids))
+        if not curr_product:
+            return False
 
-        categories_to_delete.delete(synchronize_session="fetch")
+        categories = list(map(lambda cat: int(cat), categories))
+
+        # delete the categories not selected in update
+        CategoryProduct.query \
+                       .filter(CategoryProduct.product_id == product_id) \
+                       .filter(CategoryProduct.category_id.notin_(categories)) \
+                       .delete(synchronize_session="fetch")
+
+        # everytime we access the property categories a query is made
+        # this is why we save it in a variable
+        curr_categories = list(map(lambda cat: cat.id, curr_product.categories))
 
         if len(categories) > 0:
             db.session().bulk_insert_mappings(
                 CategoryProduct,
                 [
                     {"category_id": category, "product_id": product_id}
-                    for category in categories
+                    for category in [cat for cat in categories if cat not in curr_categories]
                 ]
             )
 
