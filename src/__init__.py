40170ca23175cb9ec4aa73b29d52034a8c0bc698
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-10 20:56:45 +0300

    display unauthorized message

--- src/__init__.py
+++ src/__init__.py
@@ -15,7 +15,7 @@ Login manager
 login_manager = LoginManager()
 
 login_manager.init_app(app)
-login_manager.login_message = "Unauthorized"
+login_manager.login_message = "Ominaisuus vaatii sisäänkirjautumisen"
 login_manager.login_view = "index"
 
 from src import routes
