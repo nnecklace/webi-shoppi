37da0eb1c4eba8d6e5d4cf4714b33378b75e977c
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-13 16:26:27 +0300

    fix typo in min max amount

--- src/forms/users/balance.py
+++ src/forms/users/balance.py
@@ -3,7 +3,7 @@ from wtforms import IntegerField, validators
 from src.constants import get_required_msg, get_max_integer
 
 class BalanceForm(FlaskForm):
-    balance = IntegerField("Määrä", [validators.NumberRange(min=1, max=get_max_integer(), message="Virheellinen määrä minimi määrä 1 € - maksimimäärä " + str(get_max_integer())), validators.InputRequired(message=get_required_msg())])
+    balance = IntegerField("Määrä", [validators.NumberRange(min=1, max=get_max_integer(), message="Virheellinen määrä minimimäärä 1 € - maksimimäärä " + str(get_max_integer())), validators.InputRequired(message=get_required_msg())])
 
     class Meta:
         csrf = False
