f2a4e0a01116bcb6aa3f104943d1fb9525eb90a9
Niclas Forsman <niclas.forsman@futurice.com>
2020-06-16 22:17:36 +0300

    fix cascading delete bug in sqlite

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
 from src import app
 from src.constants import env_production, env_sqlite, env_db_url
 
@@ -8,3 +9,10 @@ if not env_production():
 app.config["SQLALCHEMY_DATABASE_URI"] = env_db_url()
 
 db = SQLAlchemy(app)
+
+if env_sqlite():
+    def _fk_pragma_on_connect(dbapi_con, con_record):
+        dbapi_con.execute('pragma foreign_keys=ON')
+
+    with app.app_context():
+        event.listen(db.engine, 'connect', _fk_pragma_on_connect)
